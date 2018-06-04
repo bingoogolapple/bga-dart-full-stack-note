2b6eec712c35c08d57265f0352bb80550fa10dab
bingoogolapple <bingoogolapple@gmail.com>
2018-06-05 00:36:03 +0800

    Flutter Widget框架概述

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_note/main.dart';
+import 'package:flutter_note/firstapp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new BGABlogApp());
+    await tester.pumpWidget(new FirstApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
