522cee0cff280a2968459b87b2018c876dfd1d9b
bingoogolapple <bingoogolapple@gmail.com>
2018-06-05 01:21:36 +0800

    在Flutter中构建布局

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'firstapp.dart';
 import 'basic_widgets.dart';
 import 'material_components.dart';
 import 'shopping.dart';
+import 'layout_one.dart';
 
 //void main() => runApp(FirstApp());
 //void main() => runApp(BasicWidgetsApp());
 //void main() => runApp(MaterialComponentsApp());
-void main() => runApp(ShoppingApp());
-
+//void main() => runApp(ShoppingApp());
+void main() => runApp(LayoutOneApp());
